ca672f115de62599a47e18dca43fae7cf37e034f
jliu670 <junminliu@bloomberg.net>
2020-03-25 20:39:05 -0400

    find in-order traveral successor of a binrary tree node

--- src/main/java/CrackingTheCodingInterview/TreeQ.java
+++ src/main/java/CrackingTheCodingInterview/TreeQ.java
@@ -9,7 +9,7 @@ public class TreeQ {
     //find a given node's in-order traveral successor
     public static Node inOrderSuccessor(Node root, Node target) {
         if(target.right == null)
-            return dfs(root, target, null, false);
+            return dfs(root, target);
         else{
             Node right = target.right;
             while(right.left!=null)
@@ -20,16 +20,21 @@ public class TreeQ {
 
     //find parent node for target node
     //and target node is left child of the parent
-    public static Node dfs(Node curr, Node target, Node parent, boolean isLeft) {
+    public static Node dfs(Node curr, Node target) {
         if(curr == null)
             return null;
         if(curr == target)
-            return isLeft? parent : null;
+            return curr;
         else{
-            Node found = dfs(curr.left, target, curr, true);
+            Node found = dfs(curr.left, target);
             if(found == null)
-                found = dfs(curr.right, target, curr, false);
-            return found;
+                return dfs(curr.right, target);
+            else{
+                if(found == target)
+                    return curr;
+                else
+                    return found;
+            }
         }
     }
 
